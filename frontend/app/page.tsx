0841cd4a60ca566b945213f1998a46c6e7ecdec8
LAPTOP-AP4DTO34\ACER <pachtnw@hotmail.com>
2026-02-19 22:08:18 +0700

    first commit

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <>
+      <div className="bg-black text-center text-white font-bold">
+        <div>Hello, World!</div>
+      </div>
+    </>
+  );
+}
