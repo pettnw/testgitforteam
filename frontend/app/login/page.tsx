e0019120bd139a25bbea31464e01f798b4bb5007
LAPTOP-AP4DTO34\ACER <pachtnw@hotmail.com>
2026-02-21 21:24:04 +0700

    add login page

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Login() {
+  return (
+    <>
+      <div className="bg-black text-center text-white font-bold">
+        <div>Login page.</div>
+      </div>
+    </>
+  );
+}
