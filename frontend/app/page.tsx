fc52370a74a1d6cd0255f0afe5207a4fe0bf4275
LAPTOP-AP4DTO34\ACER <pachtnw@hotmail.com>
2026-02-22 14:16:17 +0700

    uat on uat

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <>
       <div className="bg-black text-center text-white font-bold">
-        <div>branch uat</div>
+        <div>Hello, uat! on uat</div>
       </div>
     </>
   );
