cbb6d57a1c1de0cc6a98139e213344e92789c03b
LAPTOP-AP4DTO34\ACER <pachtnw@hotmail.com>
2026-02-22 13:51:23 +0700

    Update uat

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <>
       <div className="bg-black text-center text-white font-bold">
-        <div> i love mom!</div>
+        <div>branch uat</div>
       </div>
     </>
   );
